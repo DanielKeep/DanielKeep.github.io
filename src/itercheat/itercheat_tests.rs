6db728d9f4e1f0980bd5f70ac889d15483ba01c0
Daniel Keep <daniel.keep@gmail.com>
2017-05-17 20:47:13 +1000

    Updated iterator cheat sheet.

--- src/itercheat/itercheat_tests.rs
+++ src/itercheat/itercheat_tests.rs
@@ -1,12 +1,38 @@
 // cargo-deps: itertools
 extern crate itertools;
+use std::cmp::Ordering as O;
 use itertools::Itertools;
+use itertools::assert_equal as iae;
+use itertools::MinMaxResult;
 
 fn main() {
-    use itertools::assert_equal as iae;
-
     iae((0..4).interleave(vec![8, 9]), vec![0, 8, 1, 9, 2, 3]);
     iae((0..0).interleave(vec![8, 9]), vec![8, 9]);
+
+    assert_eq!((0..4).cmp(0..5), O::Less);
+    assert_eq!((0..4).cmp(0..4), O::Equal);
+    assert_eq!((0..5).cmp(0..4), O::Greater);
+
+    {
+        let a = vec![0.0f32].into_iter();
+        let b = vec![0.0, 1.0].into_iter();
+        assert_eq!(a.partial_cmp(b), Some(O::Less));
+    }
+    {
+        let a = vec![0.0f32, 1.0].into_iter();
+        let b = vec![0.0, 1.0].into_iter();
+        assert_eq!(a.partial_cmp(b), Some(O::Equal));
+    }
+    {
+        let a = vec![0.0f32, std::f32::NAN].into_iter();
+        let b = vec![0.0, 1.0].into_iter();
+        assert_eq!(a.partial_cmp(b), None);
+    }
+
+    {
+        let a = vec![String::from("a"), String::from("a")];
+        assert_eq!(a.into_iter().minmax(), MinMaxResult::OneElement(String::from("a")));
+    }
 
     println!("Ok.");
 }
